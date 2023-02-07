6f137a0a2e5482020c26d3ffa3c8ca7269d198dc
Maximilian Kubik <mk385@st-andrews.ac.uk>
2023-02-07 18:16:39 +0000

    Added the input reader

--- Eliza/src/IO/InputReader.java
+++ Eliza/src/IO/InputReader.java
@@ -1,3 +1,5 @@
+
+
 import java.util.Arrays;
 import java.util.List;
 import java.io.BufferedReader;
@@ -6,18 +8,25 @@ import java.io.InputStreamReader;
 
 public class InputReader {
 
-
-    public static void main(String[] args){
+    public static BufferedReader createReader(){
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
+        return br;
+    }
+
+
+    public static String getInput(BufferedReader br){
+        String input = "";
         try{
-            while(true){
-                String input = br.readLine();
-            List<String> inputList = Arrays.asList(input.split(" "));
-            System.out.println(inputList);
-            }
+            input = br.readLine();
         } catch(IOException e){
             e.getMessage();
         }
+        return input;
+    }
+
+    public static void main(String[] args){
+        BufferedReader br = createReader();
+        getInput(br);
     }
 }
